dbe8e395fd1d38b03d131dd987fddc849902f1ef
yanyixingfu <55655658+yanyixingfu@users.noreply.github.com>
2025-09-03 16:10:38 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -130,11 +130,6 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
-    },
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
